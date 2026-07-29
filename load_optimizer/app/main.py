6a18011f11742aae3ae70fe8b7d70d245d1a96d3
Steve Sherry <stevesherry123@users.noreply.github.com>
2026-07-29 12:14:38 +0100

    Schedule normal automatic runs overnight

--- load_optimizer/app/main.py
+++ load_optimizer/app/main.py
@@ -22,7 +22,7 @@ try:
 except ImportError:  # Running as /app/main.py in the Home Assistant container.
     from costing import recommend_cycle, tariff_periods_from_entity
 
-APP_VERSION = "0.8.51"
+APP_VERSION = "0.8.52"
 DISHWASHER_AUTOMATION_PACKAGE_VERSION = "0.8.51"
 MAX_PUBLISHED_COST_BREAKDOWN_ROWS = 24
 API_BASE_URL = "http://supervisor/core/api"
